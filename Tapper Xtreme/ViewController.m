6ab0e453c05633de6a8dbc8ddc5826d15402da13
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-04-11 16:59:44 -0500

    The app is now complete.

--- Tapper Xtreme/ViewController.m
+++ Tapper Xtreme/ViewController.m
@@ -10,17 +10,77 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet UITextField *inputTF;
 
+@property (weak, nonatomic) IBOutlet UITextField *inputTF;
+@property (weak, nonatomic) IBOutlet UIImageView *logo;
+@property (weak, nonatomic) IBOutlet UIButton *playBTN;
+@property (weak, nonatomic) IBOutlet UIButton *starBTN;
+@property (weak, nonatomic) IBOutlet UILabel *countLBL;
 
 @end
 
 @implementation ViewController
 
+int maxTaps = 0;
+int currentTaps = 0;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     _inputTF.delegate = self;
+    [_starBTN setHidden:YES];
+    [_countLBL setHidden:YES];
+}
+- (IBAction)playBTN:(id)sender {
+    
+    if (![_inputTF.text isEqual: @""] && ![_inputTF.text isEqual:nil]) {
+        [_inputTF setHidden:YES];
+        [_logo setHidden:YES];
+        [_playBTN setHidden:YES];
+        [_starBTN setHidden:NO];
+        [_countLBL setHidden:NO];
+        
+        maxTaps = [_inputTF.text intValue];
+        currentTaps = 0;
+        [self updateTapsLBL];
+    }
+    
+}
+
+-(void) updateTapsLBL {
+    NSString *counterLBLString = [NSString stringWithFormat:@"%d Taps", currentTaps];
+    _countLBL.text = counterLBLString;
+}
+- (IBAction)starBTNPressed:(id)sender {
+    
+    currentTaps ++;
+    [self updateTapsLBL];
+    
+    if ([self isGameOver]) {
+        
+        [self restartGame];
+    }
+    
+}
+
+-(BOOL) isGameOver {
+    if (currentTaps >= maxTaps) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+-(void) restartGame {
+    maxTaps = 0;
+    _countLBL.text = @"";
+    [_countLBL setHidden:YES];
+    [_logo setHidden:NO];
+    [_playBTN setHidden:NO];
+    [_inputTF setHidden:NO];
+    [_starBTN setHidden:YES];
+    _inputTF.text = @"";
+    
 }
 
 -(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
